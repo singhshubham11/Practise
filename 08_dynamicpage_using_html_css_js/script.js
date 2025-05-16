d308d1fcb1cb15a2a038c1ac775d2706a93bdd88
singhshubham11 <kitesmaurya@gmail.com>
2025-05-16 08:34:32 +0000

    add java script for responsive page

---
+++ 08_dynamicpage_using_html_css_js/script.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('toggle');
+});
+
+const buttons = document.querySelectorAll('.content-btn');
+const contentTitle = document.getElementById('content-title');
+const contentText = document.getElementById('content-text');
+
+const contentData = {
+    web: {
+        title: 'Web Development',
+        text: 'we create responsive, high-performance websites tailored to your needs using modern technologies like HTML, CSS, and JavaScript.'
+    },
+    design: {
+        title: 'Graphic Design',
+        text: 'Our team designs stunning visuals, including logos, banners, and UI/UX, to elevate your brand identity.'
+    },
+    marketing: {
+        title: 'Digital Marketing',
+        text: 'Boost your online presence with our SEO, social media, and content marketing strategies.'
+    }
+};
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const contentKey = button.getAttribute('data-content');
+        contentTitle.textContent = contentData[contentKey].title;
+        contentTitle.textContent = contentData[contentKey].text;
+    });
+})
