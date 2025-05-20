ea19a0a95ff54933d2c18c2f46875884d5994a58
singhshubham11 <kitesmaurya@gmail.com>
2025-05-20 13:34:59 +0000

    bring data from input box and add it in to local storage by calling getTasks()

---
+++ 09_todo_localstorage_in_browser/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    loadTasks();
+});
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value.trim();
+    
+    if (taskText === '') {
+        alert('Please enter a task!');
+        return;
+    }
+
+    const task = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    }
+}
